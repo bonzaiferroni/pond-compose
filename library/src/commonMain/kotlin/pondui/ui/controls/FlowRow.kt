e2b5136f9e91a77150843aa088c36401e6ca018a
Luke Bollwerk <lukas8@gmail.com>
2025-05-30 16:00:32 -0600

    refactor trek

--- library/src/commonMain/kotlin/pondui/ui/controls/FlowRow.kt
+++ library/src/commonMain/kotlin/pondui/ui/controls/FlowRow.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.FlowRowScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import pondui.ui.theme.Pond
 import pondui.ui.theme.Spacing
 import pondui.ui.theme.toColumnArrangement
 import pondui.ui.theme.toRowArrangement
@@ -12,13 +13,15 @@ import pondui.ui.theme.toRowArrangement
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun FlowRow(
-    spacing: Spacing,
+    unitSpacing: Int,
+    maxItemsInEachRow: Int,
     modifier: Modifier = Modifier,
     content: @Composable FlowRowScope.() -> Unit
 ) {
     FlowRow(
-        verticalArrangement = spacing.toColumnArrangement(),
-        horizontalArrangement = spacing.toRowArrangement(),
+        verticalArrangement = Pond.ruler.columnArrangement(unitSpacing),
+        horizontalArrangement = Pond.ruler.rowArrangement(unitSpacing),
+        maxItemsInEachRow = maxItemsInEachRow,
         modifier = modifier
     ) {
         content()
