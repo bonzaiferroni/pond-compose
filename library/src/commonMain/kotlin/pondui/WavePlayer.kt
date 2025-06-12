e4631a5b5cbb8996541a79d88c18844c3a59657e
Luke Bollwerk <lukas8@gmail.com>
2025-06-12 14:21:09 -0600

    generate speech

--- library/src/commonMain/kotlin/pondui/WavePlayer.kt
+++ library/src/commonMain/kotlin/pondui/WavePlayer.kt
@@ -7,7 +7,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 
 expect class WavePlayer() {
-    suspend fun play(url: String)
+    fun play(url: String)
 }
 
 @Composable
--- library/src/desktopMain/kotlin/pondui/WavePlayer.desktop.kt
+++ library/src/desktopMain/kotlin/pondui/WavePlayer.desktop.kt
@@ -1,30 +1,92 @@
 package pondui
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.ByteArrayInputStream
+import java.io.File
 import java.net.URI
-import javax.sound.sampled.AudioSystem
-import javax.sound.sampled.Clip
-import javax.sound.sampled.FloatControl
+import javax.sound.sampled.*
 import kotlin.math.log10
 
 actual class WavePlayer {
     private val clip = AudioSystem.getClip()
+    private var job: Job? = null
 
-    actual suspend fun play(url: String) {
-        if (clip.isRunning) {
-            for (i in 1..10) {
-                clip.setVolume((10 - i).toFloat())
-                delay(100)
+    init {
+        clip.addLineListener { ev ->
+            if (ev.type == LineEvent.Type.STOP) {
+                println("Stopped at frame ${clip.framePosition} of ${clip.frameLength}")
             }
         }
-        val stream = AudioSystem.getAudioInputStream(URI.create(url).toURL())
-        clip.open(stream)
-        clip.start()
+    }
+
+    actual fun play(url: String) {
+        job?.cancel()
+        job = CoroutineScope(Dispatchers.IO).launch {
+            try {
+                if (clip.isOpen) clip.close()
+                // val bis = URI.create(url).toURL().openStream().buffered()
+                val ais = if (url.startsWith("http")) {
+                    AudioSystem.getAudioInputStream(URI.create(url).toURL())
+                } else {
+                    AudioSystem.getAudioInputStream(File(url).absoluteFile)
+                }
+                clip.framePosition = 0;
+                clip.open(ais)
+                val frames     = clip.frameLength                     // total sample-frames
+                val frameRate  = clip.format.frameRate                // frames per second
+                val durationS  = frames / frameRate                   // in seconds
+                val durationMs = (durationS * 1000).toLong()          // in ms
+
+                println("Clip expects: $durationS s ($durationMs ms), frames: $frames, frameRate: $frameRate")
+                clip.loop(1)
+                delay(durationMs)
+                clip.stop()
+            } catch (e: Exception) {
+                println(e.message)
+            }
+        }
     }
 }
+//    public actual override fun powerUp() {
+//        startMixer()
+//    }
+//
+//    private fun play(url: String) {
+//
+//    }
+//
+//    private fun startMixer() {
+//        val clip = AudioSystem.getClip()
+//        CoroutineScope(Dispatchers.Default).launch {
+//            while (true) {
+//                val result = mixer.receiveCatching()
+//                when (val name = result.getOrNull()) {
+//                    null -> break
+//                    else -> {
+//                        audioInputStreams[name]?.let {
+//                            clip.open(it)
+//                            clip.start()
+//                        }
+//                    }
+//                }
+//                delay(10)
+//            }
+//        }
+//    }
+//
+//    public actual override fun powerDown() {
+//        audioInputStreams.clear()
+//        soundBytes.clear()
+//    }
+//}
 
 fun Clip.setVolume(volume: Float) {
     val gain = 20 * log10(volume.coerceIn(0.0001f, 1f))
     val control = getControl(FloatControl.Type.MASTER_GAIN) as? FloatControl ?: return
     control.value = gain
 }
+
