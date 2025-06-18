febc6f72386b6281c9c1708b571fa3fa3749841f
Luke Bollwerk <lukas8@gmail.com>
2025-06-18 10:42:04 -0600

    add questions to TrekPath

--- library/src/commonMain/kotlin/pondui/ui/core/StateModel.kt
+++ library/src/commonMain/kotlin/pondui/ui/core/StateModel.kt
@@ -4,13 +4,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 abstract class StateModel<State>(initialState: State) : ViewModel() {
     protected val _state = MutableStateFlow(initialState)
     val state = _state.asStateFlow()
     val stateNow get() = state.value
+
+    private val jobs = mutableListOf<Job>()
 
     protected fun setState(block: (State) -> State) {
         _state.value = block(state.value)
@@ -18,5 +22,14 @@ abstract class StateModel<State>(initialState: State) : ViewModel() {
 
     protected fun cancelJobs() {
         viewModelScope.coroutineContext[Job]?.cancelChildren()
+    }
+
+    protected fun <T> Flow<T>.launchCollectJob(block: (T) -> Unit) = viewModelScope.launch {
+        this@launchCollectJob.collect(block)
+    }.also { jobs.add(it) }
+
+    protected fun clearJobs() {
+        jobs.forEach { it.cancel() }
+        jobs.clear()
     }
 }
