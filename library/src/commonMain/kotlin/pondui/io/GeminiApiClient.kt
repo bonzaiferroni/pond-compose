16ea10823dec88cd33ccd7af57b7462f8f91e1d7
Luke Bollwerk <lukas8@gmail.com>
2025-08-11 08:22:16 -0600

    move GeminiApiClient

---
+++ library/src/commonMain/kotlin/pondui/io/GeminiApiClient.kt
@@ -0,0 +1,19 @@
+package pondui.io
+
+import kabinet.clients.GeminiMessage
+import kabinet.gemini.GeminiApi
+import kabinet.model.SpeechRequest
+
+class GeminiApiClient(
+    private val geminiApi: GeminiApi,
+    private val client: ApiClient = globalApiClient
+) {
+    /**
+     * Send a list o' messages to the AI and get back a response, savvy?
+     */
+    suspend fun chat(messages: List<GeminiMessage>) = client.post(geminiApi.chat, messages)
+
+    suspend fun image(request: String) = client.post(geminiApi.image, request)
+
+    suspend fun generateSpeech(request: SpeechRequest) = client.post(geminiApi.speech, request)
+}
